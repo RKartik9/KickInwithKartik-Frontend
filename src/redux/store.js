d24f6283de73d7f99bd60e3f1c7a9417cc96e600
Rohit Kumar <urskartik9@gmail.com>
2023-07-24 16:46:45 +0530

    changed server url

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,4 +16,4 @@ const store = configureStore({
 
 export default store;
 
-export const server = "https://kickinwithkartik-backend.vercel.app/api/v1";
+export const server = "https://kick-inwith-kartik-backend.vercel.app/api/v1";
